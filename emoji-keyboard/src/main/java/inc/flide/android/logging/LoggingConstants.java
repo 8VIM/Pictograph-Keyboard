958e281e5f0f6ce467d104b34245b1f81fd7ad19
Ravi Agarwal <flideravi@gmail.com>
2017-01-07 15:12:20 +0530

    Add easy logging features

---
+++ emoji-keyboard/src/main/java/inc/flide/android/logging/LoggingConstants.java
@@ -0,0 +1,11 @@
+package inc.flide.android.logging;
+
+public interface LoggingConstants {
+    String Project_Name = "Emoji Keyboard";
+    boolean ASSERT = true;
+    boolean DEBUG = true;
+    boolean ERROR = true;
+    boolean INFO = true;
+    boolean VERBOSE = true;
+    boolean WARN = true;
+}
